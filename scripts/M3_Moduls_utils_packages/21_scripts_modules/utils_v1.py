99bdd46ac53f7ff5d1a938a9b2b8d7ec5390b85e
DdyYazo <alejoyaba@gmail.com>
2024-07-04 15:44:34 -0500

    🆕 First commit from branch Python Files

--- scripts/M3_Moduls_utils_packages/21_scripts_modules/utils_v1.py
+++
@@ -1,9 +0,0 @@
-def get_population():
-
-    keys = ["col", "arg"]
-    values = ["col", "arg"]
-    return keys, values
-
-def population_by_country(data, country):
-    result = list(filter(lambda item: item['Country'] == country, data))
-    return result
